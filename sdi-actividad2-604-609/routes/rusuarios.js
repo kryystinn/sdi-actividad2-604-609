fa01a5c1e7b36164e221ae0b91a1b623b069ea1f
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-04-13 07:42:33 +0200

    Comentarios de lo anterior

--- sdi-actividad2-604-609/routes/rusuarios.js
+++ sdi-actividad2-604-609/routes/rusuarios.js
@@ -14,7 +14,9 @@ module.exports = function (app, swig, gestorBD) {
             email: req.body.email
         };
 
+        // Obtenemos la lista de usuarios con el mismo email
         gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            // Si no hay ningún email igual al introducido en la BD:
             if (usuarios == null || usuarios.length == 0) {
 
                 var pass = app.get("crypto").createHmac('sha256', app.get('clave'))
@@ -22,6 +24,7 @@ module.exports = function (app, swig, gestorBD) {
                 var passConf = app.get("crypto").createHmac('sha256', app.get('clave'))
                     .update(req.body.passwordConfirm).digest('hex');
 
+                // Se comprueba que las contraseñas son iguales:
                 if (pass === passConf) {
                     var usuario = {
                         email: req.body.email,
@@ -33,14 +36,15 @@ module.exports = function (app, swig, gestorBD) {
                         balance : 100
                     };
 
+                    // Sitodo es correcto se inserta el usuario en la BD:
                     gestorBD.insertarUsuario(usuario, function (id) {
                         if (id == null) {
-                            //res.send("Error al insertar ");
                             res.redirect("/registrarse?mensaje=Error al registrar usuario")
                         } else {
                             res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
                         }
                     });
+
                 } else
                     res.redirect("/registrarse?mensaje=Las contraseñas no coinciden");
 
@@ -51,13 +55,13 @@ module.exports = function (app, swig, gestorBD) {
     });
 
 
-// Identificarse
+        // Identificarse
         app.get("/identificarse", function (req, res) {
             var respuesta = swig.renderFile('./sdi-actividad2-604-609/views/identificacion.html', {});
             res.send(respuesta);
         });
 
-// Comprobar si existe el usuario
+        // Identificación del usuario
         app.post("/identificarse", function (req, res) {
             var pass = app.get("crypto").createHmac('sha256', app.get('clave'))
                 .update(req.body.password).digest('hex');
@@ -68,17 +72,21 @@ module.exports = function (app, swig, gestorBD) {
             };
             var admin = "admin@email.com";
 
+            // Entramos en sesión
             gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+                // Si no existe el usuario en la BD:
                 if (usuarios == null || usuarios.length == 0) {
                     req.session.usuario = null;
                     res.redirect("/identificarse" +
                         "?mensaje=Email o password incorrecto" +
                         "&tipoMensaje=alert-danger ");
 
+                // Si el usuario que se identifica NO es admin (se redirige a vista normal):
                 } else if (criterio.email != admin) {
                     req.session.usuario = usuarios[0].email;
                     res.redirect("/tienda");
 
+                 // Si el usuario que se identifica SÍ es admin (se redirige a vista de admin):
                 } else {
                     req.session.usuario = usuarios[0].email;
                     var c ={};
