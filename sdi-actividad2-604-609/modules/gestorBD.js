6c4e7030055bf8af227c4716b423055d65085b7e
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-04-13 08:42:17 +0200

    Dar de alta una oferta (falta añadirlo a "mis ofertas")

--- sdi-actividad2-604-609/modules/gestorBD.js
+++ sdi-actividad2-604-609/modules/gestorBD.js
@@ -25,6 +25,23 @@ module.exports = {
             }
         });
     },
+    insertarOferta : function(oferta, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collection = db.collection('ofertas');
+                collection.insert(oferta, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
     insertarUsuario : function(usuario, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
             if (err) {
