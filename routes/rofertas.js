387886fd7059078f04a2b659def930ebf48fc5bd
Cristina Ruiz de Bucesta <cristinaruizdebucesta@gmail.com>
2019-05-05 08:41:48 +0200

    Oferta destacada

--- routes/rofertas.js
+++ routes/rofertas.js
@@ -100,22 +100,55 @@ module.exports = function (app, swig, gestorBD) {
 
                 } else {
                     // SI la oferta es DESTACADA: descontar dinero
-                    if (cb){
+                    if (!cb) {
+                        res.redirect("/ofertas/propias");
+                    } else {
                         var criterio = {"_id": gestorBD.mongo.ObjectID(req.session.usuario._id)};
                         var usuario = {
-                            balance: parseInt(req.session.usuario.balance - 20)
+                            balance: parseInt(req.session.usuario.balance) - 20
                         };
-                        gestorBD.modificarUsuario(criterio, usuario, function (usuario) {
-                            if (usuario == null)
-                            res.send("Error al modificar usuario.");
-                        else
+                        gestorBD.modificarUsuario(criterio, usuario, function (result) {
+                            if (result == null)
+                                res.send("Error al modificar usuario.");
+                            else {
                                 req.session.usuario.balance = usuario.balance;
+                                res.redirect("/ofertas/propias");
+                            }
                         });
                     }
-                    res.redirect("/ofertas/propias");
                 }
             });
         }
+    });
+
+    // Destacar una oferta
+    app.get('/ofertas/destacar/:id', function (req, res) {
+        // PRIMERO modificamos la OFERTA a starred=true
+        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+        var oferta = {
+          starred: true
+        };
+        gestorBD.modificarOferta(criterio, oferta, function (result) {
+            if (result == null)
+                res.send("Error al modificar oferta.");
+
+            // SEGUNDO, modificamos el dinero del usuario
+            else{
+                var criterio = {"_id": gestorBD.mongo.ObjectID(req.session.usuario._id)};
+                var usuario = {
+                    balance: parseInt(req.session.usuario.balance) - 20
+                };
+                gestorBD.modificarUsuario(criterio, usuario, function (result) {
+                    if (result == null)
+                        res.send("Error al modificar usuario.");
+                    else {
+                        req.session.usuario.balance = usuario.balance;
+                        res.redirect("/ofertas/propias");
+                    }
+                });
+            }
+
+        });
     });
 
     // Comprar una oferta
@@ -158,14 +191,14 @@ module.exports = function (app, swig, gestorBD) {
                                     gestorBD.obtenerUsuarios(criterio, function (usuarios) {
                                         if (usuarios == null)
                                             res.send("Error al encontrar usuario vendedor.");
-                                        else{
+                                        else {
                                             var dineroVendedor = usuarios[0].balance;
                                             var criterio = {"email": vendedorEmail};
                                             var vendedor = {
-                                              balance: parseInt(dineroVendedor) + parseInt(precioOferta)
+                                                balance: parseInt(dineroVendedor) + parseInt(precioOferta)
                                             };
                                             gestorBD.modificarUsuario(criterio, vendedor, function (vendedor) {
-                                                if (idUser == null)
+                                                if (vendedor == null)
                                                     res.send("Error al modificar usuario vendedor.");
                                                 else {
                                                     res.redirect("/tienda");
@@ -173,7 +206,6 @@ module.exports = function (app, swig, gestorBD) {
                                             });
                                         }
                                     });
-
 
 
                                 }
